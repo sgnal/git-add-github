ded712452a871eecf7384f95562e54aa6a3b8567
DELL <3501383733@qq.com>
2022-01-27 13:26:42 +0800

    12345

--- src/com/nanjing/domain.java
+++ src/com/nanjing/domain.java
@@ -3,5 +3,6 @@ package com.nanjing;
 public class domain {
     public static void main(String[] args) {
         System.out.println("hello world github");
+        System.out.println("hello world");
     }
 }
